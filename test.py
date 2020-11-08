cdd63609d5b428540694433fbcd062cfbf3a6861
Anushka Khare <khareanushka2015@gmail.com>
2020-11-07 21:01:57 -0700

    "input code"

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-hi 
+import sys 
+def safe_input(f=None, prompt=""):
+
+    # Case:  Stdin
+    if f is sys.stdin or f is None:
+        line = input(prompt)
+    # Case:  From file
+    else:
+        break
+    return(line.strip(), True)
