cb53f11c2a5244b006946dfdfc0db5278effb9fd
Anushka Khare <71386735+vibin-eng@users.noreply.github.com>
2020-11-07 21:26:31 -0700

    Update test.py

--- test.py
+++ test.py
@@ -4,15 +4,18 @@ prompt0= input("How many assignments do you have?)
 prompt1= input("Enter your assignments and time required for that assignment")
 
 def safe_input(prompt="", list(prompt1)):
-
+    count=int(prompt0)
     # Case:  Stdin
     list1=[]
-    list2=
-    if f is sys.stdin:
-        line = input(prompt)
-        list1.append(line)
-        line1=
-    # Case:  From file
-    else:
-        break
-    return(line.strip())
+    list2=() 
+    f=True
+    for i in range(count):
+        if f is sys.stdin:
+            line = input(prompt)
+            list1.append(line)
+            line1=list2(1)
+            list1.append(line1)
+      
+        else:
+            break
+    return(list1)
