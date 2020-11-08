eb5db508e285fd104cab5857fc46582bf9addfaf
Anushka Khare <71386735+vibin-eng@users.noreply.github.com>
2020-11-07 21:18:58 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 import sys 
-def safe_input(prompt=""):
+prompt = "what times are you available to study?"
+input= 
+prompt1= "Enter your assignments and time required for that assignment"
+
+def safe_input(prompt="", list(prompt1)):
 
     # Case:  Stdin
-    if f is sys.stdin or f is None:
+    list1=[]
+    list2=
+    if f is sys.stdin:
         line = input(prompt)
+        list1.append(line)
+        line1=
     # Case:  From file
     else:
         break
-    return(line.strip(), True)
+    return(line.strip())
