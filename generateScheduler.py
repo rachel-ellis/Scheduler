8b6c22c5b2b341d87feb36082158f13ed0d4cecb
rachel-ellis <70451233+rachel-ellis@users.noreply.github.com>
2020-11-07 22:19:14 -0700

    Update generateScheduler.py

--- generateScheduler.py
+++ generateScheduler.py
@@ -5,18 +5,18 @@ from datetime import datetime
 
 def main():
   
-  # based on https://www.programiz.com/python-programming/datetime/current-time
+	# based on https://www.programiz.com/python-programming/datetime/current-time
 	currentTime = datetime.now().strftime('%H:%M')
   
 	# for simplicity (for now), only consider full hours
 	currentHour = int(currentTime.split(":")[0])
 	currentMin = int(currentTime.split(":")[1])
 
-  # round to nearest hour
+  	# round to nearest hour
 	if currentMin >= 30:
 		currentHour += 1
-  currentMin = 0
+	currentMin = 0
 
   
 if__name__== " main ":
-  main()
+	main()
